a3211ab214d405008a4fa9c7d686217f3e5fbe6b
Jose Cerqueira <josecerqueira@protonmail.com>
2024-06-23 19:48:29 +0100

    :heavy_plus_sign: feat: add more script cases

--- scripts/src/play.ts
+++ scripts/src/play.ts
@@ -4,7 +4,10 @@ import { randomBytes } from '@noble/hashes/utils';
 import invariant from 'tiny-invariant';
 import { SUI_TYPE_ARG } from '@mysten/sui/utils';
 import { bcs } from '@mysten/sui/bcs';
-import { SerialTransactionExecutor } from '@mysten/sui/transactions';
+import {
+  SerialTransactionExecutor,
+  ParallelTransactionExecutor,
+} from '@mysten/sui/transactions';
 
 const packageId =
   '0x3e440a9e534adc0e81144f7045769776b2c808b7549557b0e683a4cb8b65c341';
@@ -12,7 +15,7 @@ const packageId =
 invariant(packageId, 'Missing package id');
 
 const PAY_AMOUNTS = Array(200).fill(2);
-const NUMBER_OF_TXS = Array(2).fill(0);
+const NUMBER_OF_TXS = Array(3).fill(0);
 
 const play = () => {
   const tx = new Transaction();
@@ -44,23 +47,7 @@ const play = () => {
 
 const singlePlay = async () => {
   const now = new Date().getTime() / 1000;
-  const tx = new Transaction();
-
-  const payments = tx.splitCoins(tx.gas, [tx.pure.u64(2)]);
-  tx.setSender(keyPair.toSuiAddress());
-  tx.setGasBudget(2_000_000_000n);
-  tx.setGasPrice(2_000n);
-
-  tx.moveCall({
-    target: `${packageId}::coin_flip_v2::start_game`,
-    typeArguments: [SUI_TYPE_ARG],
-    arguments: [
-      tx.object(SUI_HOUSE),
-      tx.pure.u8(0),
-      tx.pure(bcs.vector(bcs.u8()).serialize(Array.from(randomBytes(512)))),
-      payments,
-    ],
-  });
+  const tx = play();
 
   const result = await client.signAndExecuteTransaction({
     signer: keyPair,
@@ -95,10 +82,30 @@ const submitWithSerialExecutor = async () => {
   console.log(`It took ${delay} with the Serial Executor to process 3 TXs`);
 };
 
+const submitWithParallelExecutor = async () => {
+  const now = new Date().getTime() / 1000;
+
+  const executor = new ParallelTransactionExecutor({
+    client,
+    signer: keyPair,
+    minimumCoinBalance: 2_000_000_000n,
+    initialCoinBalance: 2_000_000_000n,
+  });
+
+  await Promise.all(
+    NUMBER_OF_TXS.map(() => executor.executeTransaction(play()))
+  );
+
+  const delay = new Date().getTime() / 1000 - now;
+
+  console.log(`It took ${delay} with the Parallel Executor to process 3 TXs`);
+};
+
 (async () => {
   try {
+    // await submitWithParallelExecutor();
     await submitWithSerialExecutor();
-    await singlePlay();
+    // await singlePlay();
   } catch (e) {
     console.log(e);
   }
